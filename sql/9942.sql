491cab21a4e0826e92a3c2353d895bbcdca9be02
Kelly Martina Crotti <124708143+kellymartina@users.noreply.github.com>
2024-05-26 16:58:15 -0300

    solução #9942

---
+++ sql/9942.sql
@@ -0,0 +1,34 @@
+/*
+
+Find the Olympics with the highest number of athletes. 
+The Olympics game is a combination of the year and the season, and is found in the 'games' column. 
+Output the Olympics along with the corresponding number of athletes.
+
+Table: olympics_athletes_events
+id: int
+name: varchar
+sex: varchar
+age: float
+height: float
+weight: datetime
+team: varchar
+noc: varchar
+games: varchar
+year: int
+season: varchar
+city: varchar
+sport: varchar
+event: varchar
+medal: varchar
+    
+*/
+
+SELECT
+    games,
+    COUNT(DISTINCT id) AS athletes_count
+FROM olympics_athletes_events
+GROUP BY
+    games
+ORDER BY
+    athletes_count DESC
+LIMIT 1
